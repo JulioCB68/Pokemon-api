4643df866a8c3a5e125b23a569f673801ed1c508
Júlio César <juliobento237@gmail.com>
2023-06-19 03:19:06 -0300

    (config): configuring React Query Provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,21 @@
+"use client";
+
 import Head from "next/head";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 import { robotoMono } from "@/config/font";
 import { metaData } from "@/config/meta";
-
-import "./globals.css";
+import "@/styles/globals.css";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const queryClient = new QueryClient();
+
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Head>
         <title>{metaData.title}</title>
         <meta name="description" content={metaData.description} />
@@ -19,6 +23,6 @@ export default function RootLayout({
       <html lang="en">
         <body className={robotoMono.className}>{children}</body>
       </html>
-    </>
+    </QueryClientProvider>
   );
 }
